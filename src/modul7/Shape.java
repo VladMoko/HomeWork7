ec88441aedf163b1c1c63da9302d7f3cead95c23
VladMoko <advmoney2233@gmail.com>
2022-01-13 10:07:47 +0300

    1

--- src/modul7/Shape.java
+++ src/modul7/Shape.java
@@ -1,6 +1,6 @@
 package modul7;
 
-public abstract class Shape extends Main{
+public abstract class Shape {
 
     public abstract void name();
 }
--- src/modul7/Test.java
+++ src/modul7/Test.java
@@ -3,15 +3,16 @@ package modul7;
 public class Test{
     public static void main(String[] args) {
         Shape shape = new Circle();
-        Shape shape1 = new Square();
+        Shape square = new Square();
         Shape triangle = new Triangle();
         Shape rectangle = new Rectangle();
         Shape cone = new Cone();
 
-        shape.getShape(shape);
-        shape1.getShape(shape1);
-        triangle.getShape(triangle);
-        rectangle.getShape(rectangle);
-        cone.getShape(cone);
+        Main main = new Main();
+        main.getShape(shape);
+        main.getShape(square);
+        main.getShape(triangle);
+        main.getShape(rectangle);
+        main.getShape(cone);
     }
 }
